21e3f551a22847ad1155af9b2539e6e8233bc79a
rosa-skn <rosa.sokun@epitech.digital>
2024-10-02 14:38:54 +0200

    Create  ex_05.js

---
+++ ex_05.js
@@ -0,0 +1,9 @@
+function getHousesPrototype(){
+const Apartment= new Map();
+['Apartment 75008',90];
+['Apartment 94300',105];
+['Apartment 92300',140];
+['Apartment 75015',65];
+return Apartment;
+}
+console.log(getHousesPrototype())
