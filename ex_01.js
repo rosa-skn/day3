f0604cd3a12b525e8c9344c8ef325b73ce0f59d0
rosa-skn <rosa.sokun@epitech.digital>
2024-10-02 10:27:58 +0200

    Update ex_01.js

--- ex_01.js
+++ ex_01.js
@@ -5,8 +5,3 @@ return result;
 }
 
 console.log("displayResult(getAngryDog(5))")
-
-
-//Some code to test your function output : (you can modify it but /!\ do not deliver it /!\ ). Only deliver your function
-var result = getAngryDog(5);
-displayResult(result);
