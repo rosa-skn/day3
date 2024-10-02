2768282f392598246e3800fcf96742488d0ba095
rosa-skn <rosa.sokun@epitech.digital>
2024-10-02 23:50:57 +0200

    Update ex_09.js

--- ex_09.js
+++ ex_09.js
@@ -1,12 +1,14 @@
-function addHousing(housingList){ 
+let housingList = new Map() 
+function addHousing(housingList, newHouse, quantity) {
+    if (housingList[newHouse]) {
+        housingList.set[newHouse] += quantity;  
+    } else {
+        housingList.set[newHouse] = quantity;  
+    }
 }
-let housingList = new Map () ;
+
 addHousing ( housingList , " Flat " , 2)
 addHousing ( housingList , " Log " , 3)
 addHousing ( housingList , " Flat " , 3)
 addHousing ( housingList , " Hutch " , 2)
-displayResult ( housingList ) ;
-// Here is some code to test and display your function output:
-// (you must modify it to test out your code but /!\ do not deliver it /!\ ). Only deliver your function
-const result = new Map([["Flat", 5],["Log",3],["Hutch",2]]);
-displayResult(result);
+displayResult (housingList);
