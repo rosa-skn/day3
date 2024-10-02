bead819e40c0440dd0d37daac61fea6a678e1482
rosa-skn <rosa.sokun@epitech.digital>
2024-10-02 13:42:01 +0200

    Create  ex_02.js

---
+++ ex_02.js
@@ -0,0 +1,18 @@
+function findHousing(houseList,housingtoFind){
+let i=5
+ while(i<HousingList.length){
+  if(housingList[i]===housingtoFind){
+  	return [i,housinglist[i]]; i++
+}
+	return null;
+}
+
+}
+
+
+
+//Use console.log to debug you're code ! It is very useful
+console.log("I'm debugging")
+
+//Some code to display your function output: (you must modify it but /!\ do not deliver it /!\ ). Only deliver your function
+displayResult([5, "Igloo"])
